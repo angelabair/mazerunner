657003dd4061d44b7b4015a14231c9a36ad01dd6
danharel <cheezzywizz@gmail.com>
2016-11-16 15:03:15 -0500

    Fixed merge conflicts

--- maze_solver/maze_solver.ino
+++ maze_solver/maze_solver.ino
@@ -1,11 +1,7 @@
 
 
 // Included libraries
-<<<<<<< HEAD
 //#include <Servo.h>                  // Include Servo library -  We're not using servos
-=======
-#include <Servo.h>                    // Include Servo library
->>>>>>> e587d14dfebecb24ac9d27755b056a1be741dd6b
 #include <NewPing.h>                  // Include NewPing ultrasonic sensor library
 
 #define SONAR_NUM 3;                  // Number of sensors
@@ -17,7 +13,6 @@ unsigned long pingTimer[SONAR_NUM];   // When each pings
 unsigned int cm[SONAR_NUM];           // Store ping distances
 uint8_t currentSensor = 0;            // Which sensor is active.
 
-<<<<<<< HEAD
 //const int SPEED = 20;               // Set speed of servos
 
 // Library initialization
@@ -38,30 +33,16 @@ NewPing sonar[SONAR_NUM] = {          // Object array for sensors
 //   NewPing(trigger_pin, echo_pin, max_distance)
 //   trig pins (13, 7
 //   echo pins (
-=======
-//const int SPEED = 20;                 // Set speed of servos
 
-// Library initialization
-Servo leftServo;                      // Declare left servo
-Servo rightServo;                     // Declare right servo
-
-NewPing sonar[SONAR_NUM] = {          //Object array for sensors
-  //NewPing(trigger_pin, echo_pin, max_distance)
->>>>>>> e587d14dfebecb24ac9d27755b056a1be741dd6b
   NewPing(11, 12, MAX_DISTANCE),
   NewPing(13, 14, MAX_DISTANCE),
   NewPing(15, 16, MAX_DISTANCE)
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> e587d14dfebecb24ac9d27755b056a1be741dd6b
 void setup() {                        // Built in initialization block
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Starting...");
-<<<<<<< HEAD
 //  leftServo.attach(10);
 //  rightServo.attach(11);
 //  leftServo.write(90);              // Sets servo to mid-point
@@ -71,14 +52,6 @@ void setup() {                        // Built in initialization block
   pinMode(in_2, OUTPUT);
  
   pingTimer[0] = millis() + 75;       // First ping start in ms.
-=======
-  leftServo.attach(10);
-  rightServo.attach(11);
-  leftServo.write(90);                // Sets servo to mid-point
-  rightServo.write(90);
- 
-  pingTimer[0] = millis() + 75; // First ping start in ms.
->>>>>>> e587d14dfebecb24ac9d27755b056a1be741dd6b
   for (uint8_t i = 1; i < SONAR_NUM; i++)
     pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
 
@@ -97,7 +70,6 @@ void loop() {                         // Main loop
     }
   }
   
-<<<<<<< HEAD
 //   delay(50);
 //   angelasWay();
 //   dansWay();}
@@ -120,31 +92,6 @@ void oneSensorCycle() {               // Moves depending on sensor results
   Serial.println();
 }
 
-=======
-  // delay(50);
-  // angelasWay();
-  // dansWay();
-}
-
-void echoCheck() {                    // Checks ping echo and sets distance to array
-  if (sonar[currentSensor].check_timer())
-    cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
-}
-
-void oneSensorCycle() {               // Moves depending on sensor results
-  
-  angelasWay();
-  //dansWay();
-  for (uint8_t i = 0; i < SONAR_NUM; i++) {
-    Serial.print(i);
-    Serial.print("=");
-    Serial.print(cm[i]);
-    Serial.print("cm ");
-  }
-  Serial.println();
-}
-
->>>>>>> e587d14dfebecb24ac9d27755b056a1be741dd6b
 void angelasWay() {                   // Includes block for deadend
   if (!wallOnLeft())
     turnLeft();
@@ -191,7 +138,6 @@ boolean wallAhead() {                 // Returns if there is a wall ahead
 }
 
 void turnLeft() {                     // Makes robot turn left
-<<<<<<< HEAD
 //  servoLeft = -20
 //  servoRight = 20
 //  setLeftServoSpeed(SPEED * -1);
@@ -277,46 +223,3 @@ void stopRobot() {                     // Makes robot stop
 //void setRightServoSpeed(int speed) {  // Sets speed of right servo
 ////  rightServo.write(speed);
 //}
-=======
-  // servoLeft = -20
-  // servoRight = 20
-  //setLeftServoSpeed(SPEED * -1);
-  //setRightServoSpeed(SPEED);
-  setLeftServoSpeed(45);              // I don't know if any of these speeds actually work
-  setRightServoSpeed(135);            // Let's hope I didn't fuck up
-}
-
-void turnRight() {                    // Makes robot turn right
-  // servoLeft = 20
-  // servoRight = -20
-  //setLeftServoSpeed(SPEED);
-  //setRightServoSpeed(SPEED * -1);
-  setLeftServoSpeed(135);
-  setRightServoSpeed(45);
-}
-
-void moveForward() {                  // Makes robot go forward
-  //setLeftServoSpeed(SPEED);
-  //setRightServoSpeed(SPEED);
-  setLeftServoSpeed(0);
-  setRightServoSpeed(0);
-}
-
-void turnAround() {                   // Makes robot turnaround
-  setLeftServoSpeed(0);
-  setRightServoSpeed(180);
-}
-
-void stopRobot() {                    // Makes robot stop
-  setLeftServoSpeed(90);
-  setRightServoSpeed(90);
-}
-
-void setLeftServoSpeed(int speed) {   // Sets speed of left servo
-  leftServo.write(speed);
-}
-
-void setRightServoSpeed(int speed) {  // Sets speed of right servo
-  rightServo.write(speed);
-}135
->>>>>>> e587d14dfebecb24ac9d27755b056a1be741dd6b
